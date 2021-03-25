5c5715a1b3fb53b6881ef11d2e9f96f710895366
Adriana <am20dipi@alum.siena.edu>
2021-03-25 18:52:41 -0400

    Done.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,6 @@
 class Person < ActiveRecord::Base
   has_many :addresses
+  accepts_nested_attributes_for :addresses
+  # this replaces the setter method; this will write a method called "addresses_attributes="
 
 end
